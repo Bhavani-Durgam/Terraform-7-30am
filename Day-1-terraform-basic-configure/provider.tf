d72515ad349c4e7480226e100e4562e46859a830
Bhavani Durgam <durgambhavani799@gmail.com>
2026-03-09 17:23:21 +0530

    day

---
+++ Day-1-terraform-basic-configure/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.35.1" 
+      #version = ">4.0, <5.0"
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+provider "aws" {
+  
+}
