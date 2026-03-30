c9041834c47774f2185e46b5d9df7acae5216b17
Bhavani Durgam <durgambhavani799@gmail.com>
2026-03-30 17:10:06 +0530

    day-6

--- Day-12-Target-resource/main.tf
+++ Day-12-Target-resource/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "name" {
   
 }
 resource "aws_s3_bucket" "name" {
-    bucket = "veeraops-terraform"
+   bucket = "veeraops-terraform"
   
 }
 
--- dev1/main.tf
+++ dev1/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "name" {
     }
   
 }
+
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  tags = {
+    Name = "my-vpc"
+  }
+}
