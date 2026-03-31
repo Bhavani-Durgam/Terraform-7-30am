71dab8e16aaad9a29646b06496b4f45c81858b27
Bhavani Durgam <durgambhavani799@gmail.com>
2026-03-31 12:58:41 +0530

    day12

--- Day-12-Target-resource/main.tf
+++ Day-12-Target-resource/main.tf
@@ -3,12 +3,22 @@ resource "aws_instance" "name" {
     instance_type = "t2.micro"
   
 }
+
 resource "aws_s3_bucket" "name" {
    bucket = "veeraops-terraform"
   
 }
 
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+
+#   tags = {
+#     Name = "my-vpc"
+#   }
+# }
+
 #we can target specific resource to update or destroy by using -target option in terraform plan and apply command
 #terraform plan -target=aws_instance.name
 #if multiple resource we can use -target multiple times
 #terraform plan -target=aws_instance.name -target=aws_s3_bucket.name
+#terraform destroy -target=aws_vpc.main
--- Day-6-statelocking/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dev-s3-bucket-nareshit"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
